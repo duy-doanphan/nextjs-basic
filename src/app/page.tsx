6421dd0e494545583c2a9506c2773cc215454549
phanduy <gitphanthduy218@gmail.com>
2023-08-25 11:52:05 +0700

    css module

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import Link from 'next/link'
-
+import x from  '@/styles/app.module.css'
+import y from '@/styles/test.module.css'
 export default function Home() {
     return (
         <div>
             <ul>
                 <li>
-                    <Link href="/facebook">facebook</Link>
+                    <Link className={x['red']} href="/facebook">
+                        <span className={y['red']}>Facebook</span>
+                    </Link>
                     {/*<a href="/facebook">faceBook</a>*/}
                 </li>
                 <li>
