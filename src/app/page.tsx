e9e6b5b1e51579a5c472235d7f389375e2e8d0b8
phanduy <gitphanthduy218@gmail.com>
2023-08-27 11:01:32 +0700

    fetching data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,43 @@
+'use client'
 import Link from 'next/link'
 import x from '@/styles/app.module.css'
 import y from '@/styles/test.module.css'
 import AppTable from "@/components/app.table";
+import {useEffect} from "react";
 
 export default function Home() {
+
+    useEffect(() => {
+        fetchData()
+    }, [])
+
+    const fetchData = async () => {
+        const res = await fetch('http://localhost:8000/blogs')
+        const data = await res.json()
+        console.log(data)
+    }
+
     return (
-            <div>
-                <ul>
-                    <li>
-                        <Link className={x['red']} href="/facebook">
-                            <span className={y['red']}>Facebook</span>
-                        </Link>
-                        {/*<a href="/facebook">faceBook</a>*/}
-                    </li>
-                    <li>
-                        <a href="/youtube">youtube</a>
-                    </li>
-                    <li>
-                        <a href="/tiktok">tiktok</a>
-                    </li>
-                </ul>
-                <AppTable></AppTable>
-            </div>
+        <div>
+            <ul>
+                <li>
+                    <Link className={x['red']} href="/facebook">
+                        <span>Facebook</span>
+                    </Link>
+
+                </li>
+                <li>
+                    <Link className={x['red']} href="/youtube">
+                        <span>Youtube</span>
+                    </Link>
+                </li>
+                <li>
+                    <Link className={x['red']} href="/tiktok">
+                        <span>Tiktok</span>
+                    </Link>
+                </li>
+            </ul>
+            <AppTable></AppTable>
+        </div>
     )
 }
