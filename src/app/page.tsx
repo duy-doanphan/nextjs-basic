640110f1c62211a96ce906933bea863a2880241f
phanduy <gitphanthduy218@gmail.com>
2023-08-27 11:14:10 +0700

    config SWR

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,21 +4,24 @@ import x from '@/styles/app.module.css'
 import y from '@/styles/test.module.css'
 import AppTable from "@/components/app.table";
 import {useEffect} from "react";
+import useSWR from "swr";
 
 export default function Home() {
+    const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
-    useEffect(() => {
-        fetchData()
-    }, [])
-
-    const fetchData = async () => {
-        const res = await fetch('http://localhost:8000/blogs')
-        const data = await res.json()
-        console.log(data)
-    }
+    const {data, error, isLoading} = useSWR(
+        "http://localhost:8000/blogs",
+        fetcher, {
+            revalidateIfStale: false,
+            revalidateOnFocus: false,
+            revalidateOnReconnect: false
+        }
+    );
+    console.log(data)
 
     return (
         <div>
+            <div>{data?.length}</div>
             <ul>
                 <li>
                     <Link className={x['red']} href="/facebook">
